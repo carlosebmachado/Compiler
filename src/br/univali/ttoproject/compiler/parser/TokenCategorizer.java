b8626eef6a1ed5661e27e4dfc2482e1d31756723
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-03-28 17:48:37 -0300

    refactor: remove unused TokenCategorizer

--- src/br/univali/ttoproject/compiler/parser/TokenCategorizer.java
+++
@@ -1,8 +0,0 @@
-package br.univali.ttoproject.compiler.parser;
-
-public class TokenCategorizer {
-    private
-    public static CategorizedToken categorize(Token token) {
-        return new CategorizedToken(token.image, token.kind, token.beginLine, token.beginColumn, token.endLine, token.endColumn);
-    }
-}
